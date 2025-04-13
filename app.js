ff5b86db0ae70a4dac6e6332e0af01a673c8220d
ChowdamAshok <161301092+ChowdamAshok@users.noreply.github.com>
2025-04-13 15:35:57 +0530

    app.js

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 window.addEventListener('scroll', () => {
-    const backgroundRotate = document.querySelector('.background-rotate');
-    backgroundRotate.style.transform = `rotate(${window.scrollY / 12}deg)`;
-    backgroundRotate.style.background = window.scrollY > 10 ? 'conic-gradient(#808080)' : '';
+  const backgroundRotate = document.querySelector('.background-rotate');
+  backgroundRotate.style.transform = `rotate(${window.scrollY / 12}deg)`;
+  backgroundRotate.style.background = window.scrollY > 10 ? 'conic-gradient(#808080)' : '';
 });
 
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 3000,
-    reset: true
+  distance: '80px',
+  duration: 3000,
+  reset: true
 });
 
-sr.reveal(`#navbar, .left, .right img, .link-buttons, .about-list, .about h1, .about-image,
-   .timeline, .block-title, .box, .s-box, #Portfolio h1, .cft-card, .about h1, .skill-set h1,
-    .skill-set img, .card, .cnt-main h1, .cnt-main h2, .cnt-main h3, .cnt-btn`, {
-    interval: 400
+// From Top
+sr.reveal(`#navbar, .about h1, .timeline, .block-title, #Portfolio h1, .cnt-main h1, .cnt-main h2, .cnt-main h3, .link-buttons`, {
+  origin: 'top',
+  interval: 400
+});
+
+// From Left
+sr.reveal(`.left, #certifications h1, .about-image, .skill-set h1, .cnt-btn`, {
+  origin: 'left',
+  interval: 400
+});
+
+// From Right
+sr.reveal(`.right img, .about-list, .skill-set img, .card, .cft-card, .s-box, .box`, {
+  origin: 'right',
+  interval: 400
+});
+
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    document.body.classList.remove('loading');
+    document.querySelector('.loading-overlay').style.display = 'none';
+  }, 4000);
 });
